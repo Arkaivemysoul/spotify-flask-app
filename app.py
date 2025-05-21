9faffd6874d4dd7583b7a57aba019607615a5047
Arkaivemysoul <jadievr21310@gmail.com>
2025-05-21 17:53:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,61 @@
-from flask import Flask, jsonify, request
-from db import load_comments, save_comment_to_db
+from flask import Flask, render_template, request, redirect
+import sqlite3
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return jsonify({"message": "It works!"})
+DB_FILE = "comments.db"
 
-@app.route("/load_comments")
-def get_comments():
-    return jsonify(load_comments())
+def get_db_connection():
+    conn = sqlite3.connect(DB_FILE)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def load_comments():
+    conn = get_db_connection()
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS comments (
+            track_id TEXT,
+            user TEXT,
+            comment TEXT,
+            PRIMARY KEY(track_id, user)
+        )
+    """)
+    rows = conn.execute("SELECT * FROM comments").fetchall()
+    conn.close()
+    comments = {}
+    for row in rows:
+        tid = row["track_id"]
+        if tid not in comments:
+            comments[tid] = {}
+        comments[tid][row["user"]] = row["comment"]
+    return comments
+
+def save_comment_to_db(track_id, user, comment):
+    conn = get_db_connection()
+    conn.execute("""
+        INSERT INTO comments (track_id, user, comment)
+        VALUES (?, ?, ?)
+        ON CONFLICT(track_id, user) DO UPDATE SET comment=excluded.comment
+    """, (track_id, user, comment))
+    conn.commit()
+    conn.close()
+
+@app.route("/", methods=["GET"])
+def index():
+    tracks = [
+        {"name": "Track One", "artist": "Artist A"},
+        {"name": "Track Two", "artist": "Artist B"}
+    ]
+    comments = load_comments()
+    return render_template("index.html", tracks=tracks, comments=comments)
 
 @app.route("/save_comment", methods=["POST"])
 def save_comment():
-    data = request.get_json()
-    track_id = data.get("track_id")
-    author = data.get("author")
-    text = data.get("text")
-    if save_comment_to_db(track_id, author, text):
-        return jsonify({"status": "success"})
-    return jsonify({"status": "error"})
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-
-
-
-
+    track_id = request.form["track_id"]
+    kai_comment = request.form.get("kai_comment", "")
+    victoria_comment = request.form.get("victoria_comment", "")
+    if kai_comment:
+        save_comment_to_db(track_id, "kai", kai_comment)
+    if victoria_comment:
+        save_comment_to_db(track_id, "victoria", victoria_comment)
+    return redirect("/")
